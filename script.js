07ae539134f837efb50da7d7491ba8b7e292d478
wjimenezsenzano <wjimenezsenzano@gmail.com>
2026-03-08 20:29:08 +0000

    Added legend and hovertip info

--- script.js
+++ script.js
@@ -20,19 +20,40 @@ d3.csv("MTA_Subway_Stations.csv").then(function(data) {
     d.Longitude = +d["GTFS Longitude"];
   });
 
+  //Tooltip upon hover
+const tooltip = d3.select("body")
+  .append("div")
+  .style("position", "absolute")
+  .style("background", "white")
+  .style("padding", "5px")
+  .style("border", "1px solid gray")
+  .style("border-radius", "4px")
+  .style("font-size", "12px")
+  .style("visibility", "hidden");
+
   // Projection centered on NYC
   const projection = d3.geoMercator()
     .center([-73.94, 40.70])   // NYC approx center data points
     .scale(80000)              // zoom in
     .translate([width / 2, height / 2]);
 
+
+//set up the borough names
+const boroughs = [
+  {code:"M", name:"Manhattan"},
+  {code:"B", name:"Bronx"},
+  {code:"Q", name:"Queens"},
+  {code:"Bk", name:"Brooklyn"},
+  {code:"S", name:"Staten Island"}
+];
+
 //set up the borough colors
-    const boroughColor = {
-  "M": "#1f77b4",   // Manhattan
-  "B": "#ff7f0e",   // Bronx
-  "Q": "#2ca02c",   // Queens
-  "Bk": "#d62728",  // Brooklyn
-  "S": "#9467bd"    // Staten Island
+const boroughColor = {
+  "M": "#e41a1c",   // Manhattan - red
+  "B": "#377eb8",   // Bronx - blue
+  "Q": "#4daf4a",   // Queens - green
+  "Bk": "#984ea3",  // Brooklyn - purple
+  "S": "#ff7f00"    // Staten Island - orange
 };
 
 //Color according to Boroughs
@@ -44,7 +65,45 @@ svg.selectAll("circle")
   .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
   .attr("r", 3)
   .attr("fill", d => boroughColor[d.Borough])
-  .attr("opacity", 0.7);
+  .attr("opacity", 0.7)
 
+    //mouseover
+  .on("mouseover", (event, d) => {
+    tooltip
+      .style("visibility", "visible")
+      .text(d["Stop Name"]);
+  })
+  .on("mousemove", (event) => {
+    tooltip
+      .style("top", (event.pageY + 10) + "px")
+      .style("left", (event.pageX + 10) + "px");
+  })
+  .on("mouseout", () => {
+    tooltip.style("visibility", "hidden");
+  })
+
+//legend for map
+const legend = svg.append("g")
+  .attr("transform", "translate(20,20)");
   console.log("Stations plotted! Yeehaw!");
 });
+
+//draw the legend
+legend.selectAll("rect")
+  .data(boroughs)
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", (d,i) => i * 20)
+  .attr("width", 12)
+  .attr("height", 12)
+  .attr("fill", d => boroughColor[d.code]);
+
+legend.selectAll("text")
+  .data(boroughs)
+  .enter()
+  .append("text")
+  .attr("x", 18)
+  .attr("y", (d,i) => i * 20 + 10)
+  .text(d => d.name)
+  .attr("font-size", "12px");
